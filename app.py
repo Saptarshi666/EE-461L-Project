9f9633e5d7306ce17e0da17a49a4c4be16fc95e2
ajstiles22 <48535001+ajstiles22@users.noreply.github.com>
2022-04-13 14:50:23 -0500

    fully converted database.py to app.py
    all functions which require communication with front end have been given @app.routes, although the ones for project, hwset, dataset, are subject to change the route path based on what olivia and anish have named them. please make value names match the ones requested in the request.json call at the top of each function. functions without @app.routes are either unused by us(edit project, add funds), are initialization only functions(add base hwset, add physio sets), or are helper functions(physio db desc). If you feel a need to add or remove be sure to make the proper changes in functions such as these. As of now I am least sure of how the dataset page will work, as its really supposed to just make a button that links to the zip link saved in the database, but at the moment we don't know how to send data from the back end to the front end. Also I can implement delete project as a function with @app.route but wanted to save that until after we get other stuff working first

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from asyncio.windows_events import NULL
+# from crypt import methods
 from datetime import date
 from http import client
+import json
 from operator import contains
 from pickle import FALSE
 from sqlite3 import Date
+from unittest import result
+from numpy import printoptions
 import pymongo
 from pymongo import MongoClient
 import os
@@ -11,21 +15,25 @@ import wfdb
 import requests
 from bs4 import BeautifulSoup
 from cryptography.fernet import Fernet
-from flask import Flask, jsonify
+from flask import Flask, jsonify, session, request
 from flask.helpers import send_from_directory
+from flask_cors import CORS
 # ajstiles caWpDYgtyCH0Z1JU
 # ^^user   ^^cluster password --> mongodb info
 # change next line if you testing with your own mongodb if you want to be able to see inside
+print('hello world')
 Client=MongoClient("mongodb+srv://ajstiles:caWpDYgtyCH0Z1JU@cluster0.fvy1j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = Client['Users']
 db = Client['Projects']
 db = Client['HWSets']
 db = Client['PhysioNet']
-username = ''
-Password = ''
+current_user = ''
 auth = FALSE
 app = Flask(__name__, static_folder='./frontend/build', static_url_path="")
-fernet = Fernet.generate_key()
+CORS(app)
+# fernet = Fernet.generate_key()
+fernet = b'dGbn-lNuJMHD7BAHzxzq-6Ji158xIxMHQlGrFXit7H0='
+f = Fernet(fernet)
 # ****************** YOU MUST REMOVE INPUTS ***************8888
 # ****************** YOU MUST REMOVE INPUTS ***************8888
 # ****************** YOU MUST REMOVE INPUTS ***************8888
@@ -79,104 +87,128 @@ def add_custom_hwset(): # (projName,setName, capc, avail, auth): # comments like
         post_id = project.insert_one(post).inserted_id
         post_id
 
+@app.route("/HWManagement", methods=["GET", "POST"])
 def check_out_hw(): # (projID, setName, number):
+        params = request.get_json()
+        print(params)
+        setName, number, projID = params['HWSet Name'].strip(), params['Availability'].strip(), params['projID'].strip()
+        print(type(setName))
         # TODO what to do about logging out with sets forever?
         # any user can check in the checked out hw for a project, and ofc can check out more if available
         # hw set not like a library book
-        setName = input('check out set name- ')
         db = Client['HWSets']
         set = db[setName]
         if not set.find_one({"HWSet Name": setName}):
                 print('hwset not exists')
-                return False
+                return jsonify(result = "False")
         avail_temp = set.find_one({"HWSet Name": setName}, {"_id": 0, "Availability": 1})
         avail = avail_temp["Availability"]
         if avail == 0:
                 print('no sets currently available')
-                return False
-        number = int(input('how many- '))
+                return jsonify(result = "False")
         if number > avail:
                 print('not enough available to match your request')
                 number = avail
         set.update_one({"HWSet Name": setName}, {'$inc':{'Availability': -number}})
-        db = Client['Users']
-        user = db[username]
-        if not user.find_one({"HWSet Name": setName}):
-                user.insert_one({"HWSet Name": setName, "Current Out": number})
+        db = Client['Projects']
+        proj = db[projID]
+        if not proj.find_one({"HWSet Name": setName}):
+                proj.insert_one({"HWSet Name": setName, "Current Out": number})
+                return jsonify(result = "True")
         else:
-                user.find_one_and_update({"HWSet Name": setName}, {"$inc":{"Current Out": number}})
+                proj.find_one_and_update({"HWSet Name": setName}, {"$inc":{"Current Out": number}})
+                return jsonify(result = "True")
 
+@app.route("/HWManagement", methods=["GET", "POST"])
 def check_in_hw(): # (setName, number):
-        setName = input('check in set name- ')
-        db = Client['Users']
-        user = db[username]
-        if not user.find_one({"HWSet Name": setName}):
+        params = request.get_json()
+        print(params)
+        setName, number, projID = params['HWSet Name'].strip(), params['Availability'].strip(), params['projID'].strip()
+        print(type(setName))
+        db = Client['Projects']
+        proj = db[projID]
+        if not proj.find_one({"HWSet Name": setName}):
                 print('hwset not checked out')
-                return False
-        out_temp = user.find_one({"HWSet Name": setName}, {"_id": 0, "Current Out": 1})
+                return jsonify(result = "False")
+        out_temp = proj.find_one({"HWSet Name": setName}, {"_id": 0, "Current Out": 1})
         current_Out = out_temp["Current Out"]
-        number = int(input('how many- '))
         if number > current_Out:
                 print('this is more than you have checked out, try again')
-                return False
-        user.update_one({"HWSet Name": setName}, {'$inc':{'Current Out': -number}})
-        out_temp = user.find_one({"HWSet Name": setName}, {"_id": 0, "Current Out": 1})
+                return jsonify(result = "False")
+        proj.update_one({"HWSet Name": setName}, {'$inc':{'Current Out': -number}})
+        out_temp = proj.find_one({"HWSet Name": setName}, {"_id": 0, "Current Out": 1})
         current_Out = out_temp["Current Out"]
         if current_Out == 0:
                 print('all units of this set checked back in')
-                user.find_one_and_delete({"HWSet Name": setName})
+                proj.find_one_and_delete({"HWSet Name": setName})
         db = Client['HWSets']
         set = db[setName]
         set.update_one({"HWSet Name": setName}, {'$inc':{'Availability': number}})
+        return jsonify(result = "True")
 
+# *********** for checking how global current_user works *******************************8
+@app.route("/sayHi", methods=["GET", "POST"])
+def sayHi():
+        return 'Hello there '+ current_user # jsonify(result = "True")
+
+@app.route("/NewProj", methods=["GET", "POST"])
 def add_project(): # (,projName, desc, auth):
         # TODO remove inputs
         # maybe make a method to pull name from project db based on if user has the corresponding projID attatched to their acct
         # so that it can always be displayed
         # either this or, as i did in delete_project(), simply add name to each projDoc in its members and update name, desc if its ever changed etc.
-        projName = input("Project Name- ") # remove inputs when receiving data from front end
+        params = request.get_json()
+        print(params)
+        projName, projDesc, projID, funds = params['projName'].strip(), params['projDesc'].strip(), params['projID'].strip(), params['funds'].strip()
+        print(type(projName))
+        # change below so that unique projects names only apply to individual users. is it even necessary if differing ID's and Descs? prob not
+        # db = Client['Users']
+        # user = db[current_user]
+        # if user.find_one({"_id": projID}):
+        #         print('project name already in use')
+        #         return jsonify(result = "False")
         db = Client['Projects']
         project = db[projName]
-        if project.find_one({"Project Name": projName}):
-                print('project name already in use')
-                return False
-        desc = input("project description- ")
-        projID = input('project id- ')
         if project.find_one({"Project ID": projID}):
-                print('project id already exists')
-                return False
+                print('project already exists')
+                return jsonify(result = "False")
         post = {
                 "Project Name": projName,
-                "Description": desc,
-                "Project ID": projID,
-                "Members": [username]
+                "Description": projDesc,
+                "_id": projID,
+                "Funds": funds,
+                "Members": [current_user]
                 }
         # consider removing the user push, instead only pushing project ID
-        project = db[projName]
+        project = db[projID]
         post_id = project.insert_one(post).inserted_id
         post_id
         db = Client['Users']
-        user = db[username]
+        user = db[current_user]
         user.insert_one({"_id": projID})
-        return True
+        return jsonify(result = "True")
 
+@app.route('/ExistingProject/<user>', methods=["GET", "POST"])
 def join_project(): # (projID):
-        projID = input("join project enter ID- ")
+        params = request.get_json()
+        print(params)
+        projID = params['projID'].strip()
+        print(type(projID))
         db = Client['Users']
-        user = db[username]
+        user = db[current_user]
         if user.find_one({"_id": projID}):
                 print('project already joined')
-                return False
+                return jsonify(result = "False")
         db = Client['Projects']
         project = db[projID]
         if not project.find_one({"Project ID": projID}):
                 print('project not exists')
-                return False
-        project.find_one_and_update({"Project ID": projID}, {"$addToSet": {"Members": username}})
+                return jsonify(result = "False")
+        project.find_one_and_update({"Project ID": projID}, {"$addToSet": {"Members": current_user}})
         db = Client['Users']
-        user = db[username]
+        user = db[current_user]
         user.insert_one({"_id": projID})
-        return True
+        return jsonify(result = "True")
 
 def edit_project(): #(projID):
         # dont know if we want this yet, basically can only edit title and desc anyway
@@ -187,7 +219,7 @@ def edit_project(): #(projID):
 def delete_project(): # (projID):
         projID = input('delete project id- ')
         db = Client['Users']
-        user = db[username]
+        user = db[current_user]
         if not user.find_one({"_id": projID}):
                 print('project not exists')
                 return False
@@ -205,65 +237,89 @@ def delete_project(): # (projID):
                 user.find_one_and_delete({"_id": projID})
         return True
 
-@app.route('/newuser/<user>/<passw>')
-def make_user(user,passw):
+@app.route('/newuser', methods=['GET', 'POST'])
+def make_user():
+        print('make user')
         # TODO remove INPUTS when receiving data from front end
         # check for strength of pw? maybe for the future
         # add encryption
+        params = request.get_json()
+        print(params)
+        username, password = params['username'].strip(), params['password'].strip()
+        print(type(username))
+
         db = Client['Users']
-        if db.list_collection_names().__contains__(user):
+        if db.list_collection_names().__contains__(username):
                 print('username unavailable')
-                return False
-        global username
-        username = fernet.encrypt(user)
-        global Password
-        Password = fernet.encrypt(passw)
+                return jsonify(result = 'False')
+        token = f.encrypt(username.encode())
+        print(token)
+        token2 = f.encrypt(password.encode())
+        print(token2)
         post = {
-                "Username": username,
-                "Password": Password,
-                "Funds": 100
+                "Username": token,
+                "Password": token2
                 }
-        user = db[user]
+        user = db[username]
         post_id = user.insert_one(post).inserted_id
         post_id
         print('Welcome ' + username + '!')
-        return True
+        global current_user
+        current_user = username
+        return jsonify(result = "True")
 
-def add_Funds():
-        print('are we doing funds?')
-
-@app.route('/login/<user>/<passw>')
-def check_user_password(user, passw):
+@app.route('/login', methods=["GET", "POST"])
+def check_user_password():
+        print('check user')
         # TODO add reverse encryption
         # add limit on attempts? attempts per user or machine?
-        global username
-        username = fernet.encrypt(username)
-        global Password
-        Password = fernet.encrypt(passw)
+        print(fernet)
+        params = request.get_json()
+        print(params)
+        username, password = params['username'].strip(), params['password'].strip()
+        print(type(username))
         db = Client['Users']
-        userCheck = db[user]
-        if not userCheck.find_one({"Username": user, "Password": passw}):
-                print('login credentials incorrect')
-                return False
+        user = db[username]
+        if not db.list_collection_names().__contains__(username):
+                print("no such user exists")
+                return jsonify(result = "True")
+        enc_user = user.find_one()
+        print("enc_user: ")
+        print(enc_user)
+        enc_username = enc_user["Username"]
+        print(enc_username)
+        dec_username = f.decrypt(enc_username).decode()
+        print(dec_username)
+        print(username)
+        enc_password = enc_user["Password"]
+        print(enc_password)
+        dec_password = f.decrypt(enc_password).decode()
+        print(dec_password)
+        print(password)
+        if username != dec_username or password != dec_password:
+                print("login credentials incorrect")
+                return jsonify(result = "True")
         else:
-                username = user
                 print('Welcome ' + username + ' !')
-                return True
+                global current_user
+                current_user = username
+        return jsonify(result = "True")
 
 def log_out():
         # add tracker of checkouts to mass checkin upon logout
-        global username
-        username = ''
+        global current_user
+        current_user = ''
 
 # PhysioNet Citation:
 # Goldberger, A., Amaral, L., Glass, L., Hausdorff, J., Ivanov, P. C., Mark, R., ... & Stanley, H. E. (2000). PhysioBank, PhysioToolkit, and PhysioNet: Components of a new research resource for complex physiologic signals. Circulation [Online]. 101 (23), pp. e215–e220.
+# @app.route("/PublicDatasets", methods=["GET", "POST"])
+# can just run this as initialization with 5 predetermined set abbreviations, doesn't really need user input
 def add_physio_db(pdbAbbrev): # (pdbAbbrev, pdbDesc): 
         # small sizes are aami-ec13, bpssrat, need 3 more
         # TODO no duplicates
         # TODO add descriptions
         # maybe add counter for number of downloads?
         dbs = wfdb.get_dbs()
-        # pdbAbbrev = input('enter pdb- ')
         for i in range(len(dbs)):
                 if dbs[i][0] == pdbAbbrev:
                         pdbTitle = str(dbs[i][1])
@@ -274,7 +330,7 @@ def add_physio_db(pdbAbbrev): # (pdbAbbrev, pdbDesc):
                 return False
         if not physio_db_exists(pdbAbbrev):
                 print('dataset does not exist')
-                return
+                return False
         pdbDesc = "curse you and your lack of uniform formatting physionet"
         db = Client["PhysioNet"]
         pdb = db[pdbAbbrev]
@@ -300,6 +356,7 @@ def physio_db_exists(pdbAbbrev):
 # change it to webscrape for the zip url still possible tho
 def physio_db_zip(pdbAbbrev):
     # ".zip link": "https://physionet.org <+<breaks here>+> /static/published-projects/adfecgdb/abdominal-and-direct-fetal-ecg-database-1.0.0.zip" no spaces ofc
+    # pdbAbbrev = 'aami-ec13'
     URL = "https://physionet.org/content/" + pdbAbbrev + "/1.0.0/"
     resp = requests.get(URL)
     if resp.status_code==200:
@@ -309,6 +366,7 @@ def physio_db_zip(pdbAbbrev):
         z = str(abs_head.find_next())
         zip_ext = z[13:-41]
         zippy = "https://physionet.org" + zip_ext
+        print(zippy)
         return zippy
     else:
         print('no zip link')
@@ -316,24 +374,31 @@ def physio_db_zip(pdbAbbrev):
 
 def physio_db_desc(pdbAbbrev):
         if pdbAbbrev == 'aami-ec13':
-                desc = "desc"
+                desc = "yes desc"
                 return desc
         if pdbAbbrev == 'bpssrat':
                 desc = "rat"
                 return desc
         return 'no desc'
 
-def request_physio_zip(pdbAbbrev):
+
+@app.route('/zipfile')
+def request_physio_zip():
+        print(fernet)
+        params = request.get_json()
+        print(params)
+        pdbAbbrev = params['_id'].strip()
+        print(type(pdbAbbrev))
         db = Client["PhysioNet"]
         pdb = db[pdbAbbrev]
         if db.list_collection_names().__contains__(pdbAbbrev):
                 zip_link = pdb.find_one({"_id": pdbAbbrev})
                 zip = zip_link["Zip Link"]
                 print(zip)
-                return True
+                return jsonify(result = "True")
         else:
                 print('dataset not exists')
-                return False
+                return jsonify(result = "False")
 
 # below downloads individual files to computer, idk if really needed tho
 # needs to be called in if __name__ == "__main__": function or it will time out and be stuck in loop forever
@@ -345,44 +410,16 @@ def download_physio_db(): # (pdbAbbrev): the abbreviation for the physioDatabase
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
-# add_base_hwset()
-# *********** TESTING WITH INPUTS REMOVE BELOW
-# print(db.list_collection_names())
-# if not make_user('alberto', 'pass'):
-        # check_user_password('alberto', 'pass')
-# if __name__ == "__main__":
-#         download_physio_db()
+    return send_from_directory(app.static_folder, "index.html")
+
+add_base_hwset()
 add_physio_db('aami-ec13')
 add_physio_db('bpssrat')
 add_physio_db('wrongwrongwrong') # meant to fail
 add_physio_db('adfecgdb')
 add_physio_db('aftdb')
 add_physio_db('norwegian-athlete-ecg')
-request_physio_zip('aami-ec13')
-request_physio_zip('bpssrat')
-request_physio_zip('adfecgdb')
-request_physio_zip('aftdb')
-request_physio_zip("norwegian-athlete-ecg")
-# add_project()
 # log_out()
-# if not make_user('alberto2', 'pass2'):
-#         check_user_password('alberto2', 'pass2')
-# add_project()
-# join_project()
-# check_out_hw()
-# check_in_hw()
-# print('make next project ID is delete no spaces')
-# delete_project()
-# add_custom_hwset()
-log_out()
-# *********** END OF MANUAL TEST DONT REMOVE STUFF PAST HERE
-
-Client.close()
+# Client.close()
 if __name__ == '__main__':
     app.run()
-# TODO
-# encrypt password (HW1) cypher
-# get info from ui 
-# lock user info behind password 
-# physio??? 5 sets, have link to download in mongodb
